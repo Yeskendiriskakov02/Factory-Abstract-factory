7e74b653c0b40975afb49dbb1d37dc7562288fd4
Yeskendiriskakov02 <91181514+Yeskendiriskakov02@users.noreply.github.com>
2021-10-20 22:07:06 +0600

    Create ConsumerClass.java

---
+++ AbstractFactory/ConsumerClass.java
@@ -0,0 +1,17 @@
+public class ConsumerClass {
+    public AbstractFood placeOrder(String itemName,int quantity,String itemType) {
+        AbstractFoodFactory a = null;
+        if(itemType.equals("italian")) {
+            a = new ItalianFoodFactory();
+        }else if(itemType.equals("mexican")) {
+            a = new MexicanFoodFactory();
+        }else if(itemType.equals("chinese")) {
+            a = new ItalianFoodFactory();
+        }
+        if(a!=null) {
+            return a.placeOrder(itemName, quantity);
+        }else {
+            return null;
+        }
+    }
+}
